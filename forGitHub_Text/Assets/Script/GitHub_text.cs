944ea5abb3ecd2ac901ec0292fc8f5b543ce535e
MrW117 <87253728+MrW117@users.noreply.github.com>
2021-07-12 21:22:56 +0800

    makeScriptFile

---
+++ forGitHub_Text/Assets/Script/GitHub_text.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitHub_text : MonoBehaviour
+{
+
+    public int x = 10;
+    public int y = 20;
+
+    private void Start()
+    {
+        //////////////////////////
+        //用于测试修改的代码，原来是x就改为y，原来是y就改为x
+        Debug.Log(x);
+
+        //////////////////////////
+    }
+
+}
